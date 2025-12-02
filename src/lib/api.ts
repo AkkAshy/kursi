f828826cb76001db5181dc423c02c9d08494748b
AkkAshy <asirepovakkanat@gmail.com>
2025-12-02 23:56:36 +0500

    fix290

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,9 @@
 import axios, { AxiosInstance, InternalAxiosRequestConfig } from 'axios'
 import { AuthTokens, User, Course, BotLead, CreatorProfile, Lesson, Plan, Subscription, SubscriptionUsage, SubscriptionPayment, PaymentSettings, ManualPayment, ChangePlanResponse } from '@/types'
 
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000/api'
+// const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000/api'
+const API_BASE_URL = 'https://kursi.erp-imaster.uz/api'
+
 
 class ApiClient {
   private client: AxiosInstance
